69ca7659bdd4ce887d1778d531d7d88fe1b1ad20
strongmhk <kim6562166086@gmail.com>
2024-02-17 02:24:27 +0900

    chore: 시큐리티 비활성화

---
+++ gamoon/src/test/java/com/gamoon/GamoonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gamoon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = GamoonApplicationTests.class)
+class GamoonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
