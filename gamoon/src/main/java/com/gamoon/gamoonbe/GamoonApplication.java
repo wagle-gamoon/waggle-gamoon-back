09bb62af2a6a6e3e33564b56329fe9a768bd51bb
DongHoon Lee <dhl1924@naver.com>
2024-02-17 06:57:00 +0900

    refactor: EnableJpaAuditing 추가

--- gamoon/src/main/java/com/gamoon/gamoonbe/GamoonApplication.java
+++ gamoon/src/main/java/com/gamoon/gamoonbe/GamoonApplication.java
@@ -3,6 +3,7 @@ package com.gamoon.gamoonbe;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
