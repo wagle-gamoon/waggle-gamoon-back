e0d394ddd7a11d99f6e0b6c211e0c00ffc5f7edc
strongmhk <kim6562166086@gmail.com>
2024-02-16 18:25:50 +0900

    initial commit

---
+++ gamoon/src/main/java/com/example/gamoon/GamoonApplication.java
@@ -0,0 +1,13 @@
+package com.example.gamoon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamoonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamoonApplication.class, args);
+	}
+
+}
---
+++ gamoon/src/test/java/com/example/gamoon/GamoonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gamoon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamoonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
