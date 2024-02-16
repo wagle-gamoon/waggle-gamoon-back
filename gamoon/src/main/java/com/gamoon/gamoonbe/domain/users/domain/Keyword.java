f06fd808e1d07f7830fe3f146bacca87aa3d8df1
DongHoon Lee <dhl1924@naver.com>
2024-02-17 01:29:30 +0900

    refactor: 키워드 추가

--- gamoon/src/main/java/com/gamoon/gamoonbe/domain/users/domain/Keyword.java
+++ gamoon/src/main/java/com/gamoon/gamoonbe/domain/users/domain/Keyword.java
@@ -6,7 +6,12 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum Keyword {
-    인기짱("인기짱");
+    대면("대면"),
+    비대면("비대면"),
+    조언자("조언자"),
+    경청자("경청자"),
+    일회성("일회성"),
+    꾸준히("꾸준히");
 
     private final String keyword;
 }
