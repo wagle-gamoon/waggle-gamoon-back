13f999b74c67d899ea43ba17f7ae750b82984edf
DongHoon Lee <dhl1924@naver.com>
2024-02-17 02:13:07 +0900

    hotfix: build falied 원인 수정

--- gamoon/src/test/java/com/gamoon/gamoonbe/GamoonApplicationTests.java
+++ gamoon/src/test/java/com/gamoon/gamoonbe/GamoonApplicationTests.java
@@ -3,7 +3,7 @@ package com.gamoon.gamoonbe;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = GamoonApplicationTests.class)
 class GamoonApplicationTests {
 
 	@Test
